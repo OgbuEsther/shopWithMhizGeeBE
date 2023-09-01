384261464a0571969c67956975e656e82435db46
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-09-01 15:59:14 +0100

    God Abeg

--- controller/ProductController.ts
+++ controller/ProductController.ts
@@ -6,7 +6,9 @@ import jwt from "jsonwebtoken";
 const router = express.Router();
 import cloudinary from "../utils/cloudinary";
 import { uploadProducConfig } from "../utils/multer";
-import { makePayment, payOut } from "./payWithFlutter";
+import {  payOut } from "./payWithFlutter";
+import Flutterwave from "flutterwave-node-v3";
+import axios from "axios";
 // create product
 
 router.post("/new-product",uploadProducConfig, async (req: Request, res: Response) => {
@@ -104,5 +106,64 @@ router.get("/allproducts/:id", async (req: Request, res: Response) => {
 
 router.post("/payOut" , payOut)
 // router.post("/payOut" , makePayment)
+
+router.get("/payment-callback", async (req: Request, res: Response) => {
+  try {
+    const { amount } = req.body;
+    const response = await axios.post(
+
+      "https://api.flutterwave.com/v3/payments",
+      {
+        headers: {
+          Authorization: `Bearer FLWSECK_TEST-8e72e4f893620e8e9cdb06e6ca76bf14-X`,
+        },
+        json: {
+          tx_ref: "hooli-tx-1920bbtytty",
+          amount: amount,
+          currency: "NGN",
+          redirect_url:
+            "https://webhook.site/9d0b00ba-9a69-44fa-a43d-a82c33c36fdc",
+          meta: {
+            consumer_id: 23,
+            consumer_mac: "92a3-912ba-1192a",
+          },
+          customer: {
+            email: "user@gmail.com",
+            phonenumber: "080****4528",
+            name: "Yemi Desola",
+          },
+          customizations: {
+            title: "Pied Piper Payments",
+            logo: "http://www.piedpiper.com/app/themes/joystick-v27/images/logo.png",
+          },
+        },
+      },
+      // console.log("this is first reponse" , response)
+      
+    );
+    if (req.query.status === "successful") {
+      // const transactionDetails = await Transaction.find({ref: req.query.tx_ref});
+      const response = await Flutterwave.Transaction.verify({
+        id: req.query.transaction_id,
+      });
+      if (
+        response.data.status === "successful" &&
+        response.data.amount === amount &&
+        response.data.currency === "NGN"
+      ) {
+        // Success! Confirm the customer's payment
+        return res.status(200).json({
+          message : "Successfull",
+          data : response
+        })
+      } else {
+        // Inform the customer their payment was unsuccessful
+      }
+    }
+  } catch (err: any) {
+    console.log(err.code);
+    console.log(err.response.body);
+  }
+});
 
 export default router;
